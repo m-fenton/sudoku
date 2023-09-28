b765071dcee43b3f64a2a9498a1bff1819ba66da
mfenton <martin_fenton@hotmail.co.uk>
2023-09-28 15:33:33 +0100

    completed sudoku project

---
+++ sudoku.go
@@ -0,0 +1,157 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	if len(os.Args) != 10 {
+		fmt.Printf("Error\n")
+	} else {
+
+		if len(os.Args) != 1 {
+			if len(os.Args) == 10 || len(os.Args[1]) == 89 {
+				unstringed := ArgsValid(os.Args)
+				board := parseInput(unstringed)
+				if backtrack(&board) {
+					printBoard(board)
+				} else {
+					fmt.Printf("Error\n")
+				}
+			}
+		} else {
+			fmt.Printf("Error\n")
+		}
+	}
+}
+
+func ArgsValid(args []string) string {
+	validString := ""
+	for i := 1; i < len(args); i++ {
+		for _, nb := range args[i] {
+			if nb != ' ' {
+				validString += string(nb)
+			}
+		}
+	}
+	return validString
+}
+
+func backtrack(board *[9][9]int) bool {
+	if !hasEmptyCell(board) {
+		return true
+	}
+	for i := 0; i < 9; i++ {
+		for j := 0; j < 9; j++ {
+			if board[i][j] == 0 {
+				for candidate := 9; candidate >= 1; candidate-- {
+					board[i][j] = candidate
+					if isBoardValid(board) {
+						if backtrack(board) {
+							return true
+						}
+						board[i][j] = 0
+					} else {
+						board[i][j] = 0
+					}
+				}
+				return false
+			}
+		}
+	}
+	return false
+}
+
+func hasEmptyCell(board *[9][9]int) bool {
+	for i := 0; i < 9; i++ {
+		for j := 0; j < 9; j++ {
+			if board[i][j] == 0 {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func isBoardValid(board *[9][9]int) bool {
+
+	//check duplicates by row
+	for row := 0; row < 9; row++ {
+		counter := [10]int{}
+		for col := 0; col < 9; col++ {
+			counter[board[row][col]]++
+		}
+		if hasDuplicates(counter) {
+			return false
+		}
+	}
+
+	//check duplicates by column
+	for row := 0; row < 9; row++ {
+		counter := [10]int{}
+		for col := 0; col < 9; col++ {
+			counter[board[col][row]]++
+		}
+		if hasDuplicates(counter) {
+			return false
+		}
+	}
+
+	//check 3x3 section
+	for i := 0; i < 9; i += 3 {
+		for j := 0; j < 9; j += 3 {
+			counter := [10]int{}
+			for row := i; row < i+3; row++ {
+				for col := j; col < j+3; col++ {
+					counter[board[row][col]]++
+				}
+				if hasDuplicates(counter) {
+					return false
+				}
+			}
+		}
+	}
+
+	return true
+}
+
+func hasDuplicates(counter [10]int) bool {
+	for i, count := range counter {
+		if i == 0 {
+			continue
+		}
+		if count > 1 {
+			return true
+		}
+	}
+	return false
+}
+
+func printBoard(board [9][9]int) {
+	for row := 0; row < 9; row++ {
+		for col := 0; col < 9; col++ {
+			fmt.Printf("%d ", board[row][col])
+		}
+		fmt.Println()
+	}
+}
+
+func parseInput(input string) [9][9]int {
+	board := [9][9]int{}
+	scanner := bufio.NewScanner(strings.NewReader(input))
+
+	scanner.Split(bufio.ScanRunes)
+
+	for row := 0; row < 9; row++ {
+		for col := 0; col < 9; col++ {
+			scanner.Scan()
+			i1, _ := strconv.Atoi(scanner.Text())
+			board[row][col] = i1
+		}
+	}
+	return board
+}
